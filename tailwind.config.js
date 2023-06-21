86db927bced446d70bc95f7cea8fd7630260f5f9
nva112k22 <annguyen1112k2@gmail.com>
2023-06-21 16:04:57 +0700

    commit second

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/customers/*.liquid',
-    './templates/*.liquid',
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      height: {
+        '94': '22rem'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
